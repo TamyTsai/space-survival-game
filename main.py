aba482dbbc143511be366f52b9011d1bc3f720e3
MSI\tamy8 <tamy8677@gmail.com>
2024-05-19 19:03:22 +0800

    basic game structure

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 import pygame
 
+# 慣例上會把設定好就不會在遊戲中變動的值的變數名稱 以大寫命名
+FPS = 60 # 一秒鐘更新60次畫面
+WHITE = (255,255,255) # 視窗背景顏色
+WIDTH = 500 # 視窗寬度
+HEIGHT = 600 # 視窗高度
+
 # 遊戲初始化 及 創建視窗
 pygame.init() #pygame函式，將遊戲初始化
-screen = pygame.display.set_mode((500,600)) 
+screen = pygame.display.set_mode((WIDTH,HEIGHT)) 
 # pygame函式，創建視窗 
 # 會傳入一元組(以小括弧包住) 
 # 元組中第一個資料為寬度，第二個為高度 
 # 元組與列表之差異--元組一旦創建，不能做新增、修改或刪除--防止資料被意外修改
 # python宣告變數不用寫var
-# 遊戲迴圈:取得輸入->更新遊戲->渲染畫面 不斷重複，此處尚未寫迴圈(底下的其他城市碼)，所以程式運行時，視窗會出現後馬上關掉(後面沒有程式碼要執行了)
+# 遊戲迴圈:取得輸入->更新遊戲->渲染畫面，迴圈結束後，經過一段時間，再跑下一次迴圈，此處尚未寫迴圈(底下的其他程式碼)，所以程式運行時，視窗會出現後馬上關掉(後面沒有程式碼要執行了)
+pygame.display.set_caption("第一個遊戲") # 視窗標題
+clock = pygame.time.Clock() # 創建物件，此物件可對時間做管理與操縱
+# 每個人電腦效能不同，故遊戲迴圈結束後要等待下一次迴圈的時間不同，故要做時間管理
 
+running = True # 遊戲是否進行中(以running變數存放此狀態)
+
+# 遊戲迴圈
+while running: #條件為真時，執行以下程式碼 #若遊戲進行中:
+    clock.tick(FPS) # 一秒鐘之內最多只會執行FPS次，這樣即使有人電腦跑超快，最多也只能在一秒鐘內跑FPS次(一秒鐘更新FPS次畫面)
+    # 取得輸入
+    for event in pygame.event.get(): 
+        # pygame.event.get()會回傳現在發生的所有事件(列表)(如:滑鼠移動到哪、輸入甚麼按鍵)
+        # for迴圈會到 此事件陣列中 對每個資料執行以下程式碼
+        # Python for迴圈寫法類似Ruby，都與js不同
+        if event.type == pygame.QUIT: # 檢查 事件陣列中 事件內容 是否為 將遊戲關閉(使用者點擊視窗右上角叉叉)
+            running = False # 跳出迴圈(迴圈執行條件為runnung為true)
+
+    # 更新遊戲
+
+    # 畫面顯示
+    screen.fill(WHITE) # screen是前面創建的視窗變數 # full函式裡面要放代表顏色的元組(RGB)，會用該顏色填滿畫面
+    pygame.display.update() # 更新畫面
+
+pygame.quit() # 既然跳出迴圈，表示遊戲進行狀態running為false，故進行關閉視窗函數
